0ea17fad0ae97660d585726254cc5db922f444fd
mlane3 <mlane3@gatech.edu>
2018-03-01 05:35:51 -0500

    Update overall constrants.csv
    fixing a slight bug, Puru you should have this fixed in the orginal .csv but if not I will e-mail you a new version asap.
    2) Also I am moving the modeling files to help better organize things on github.
    3) Also United.R now contains better sliders based on what we have coded so far for CWBI.  They will allow a slider based on the range of orginal data and an improvement.  Though this makes 14*2 sliders we need to do.

--- United.R
+++ United.R
@@ -6,6 +6,7 @@
 
 # NECESSARY PACKAGES ----------------------------------------------------
 library(shiny)
+library(flexdashboard)
 library(shinydashboard)
 source("UW_R_Script_final.R")
 source('coefficents.R')
@@ -24,15 +25,16 @@ minCWB_Z = min(df_index$CWB_Z) #-1.969282
 maxCWB_Z = max(df_index$CWB_Z) #1.380706
 df2 <- as.data.frame(read.csv("overall constrants.csv", skip = 2, row.names = 1)) 
 
-"===========================================
-                HEADER
-==========================================="
+# "===========================================
+#                 HEADER -----
+# ==========================================="
 header = dashboardHeader(title = 'United Way App')
 
-"===========================================
-                SIDEBAR
-==========================================="
-choices <- unique("overall",df0$county)
+# "===========================================
+#                 SIDEBAR
+# ==========================================="
+choices <- unique(c("overall",df0$county))
+mynames <- names(df2)
 sidebar = dashboardSidebar(
 
   sidebarMenu( 
@@ -40,45 +42,61 @@ sidebar = dashboardSidebar(
                           icon = icon("th"),
                           selectInput(inputId = 'county',
                                       label = "",
-                                      choices = c("Michael",
-                                                  "Purush",
-                                                  "Sifael"))
+                                      choices = choices)
                           
                          ),
+                menuItem( text = "Select Variable (just a demo)",
+                          icon = icon("th"),
+                          selectInput(inputId = 'county',
+                                      label = "",
+                                      choices = mynames)
+                          
+                )
     
-                sliderInput("lbw",
-                            "Low Weight Births",
-                            min = df2$lwb[1],
-                            max = df2$lwb[2],
-                            value = df2$lwb[3])
               ),
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("lbw",
-                  "Low Weight Births",
-                  min = df2$lwb[1],
-                  max = df2$lwb[2],
-                  value = df2$lwb[3])
-    ),
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
-    )
-  )
+  sliderInput("lwb",
+              "Low Weight Births Range",
+              min = df2$lwb[1],
+              max = df2$lwb[2],
+              value = c(df2$lwb[1],df2$lwb[3]),
+              sep ="",step = .01),
+  sliderInput("lwbfinal",
+              "Low Weight Births Improvement",
+              min = df2$lwb[1],
+              max = df2$lwb[2],
+              value = df2$lwb[3],
+              sep ="",step = .01)
 )
 
-"===========================================
-                  BODY
-==========================================="
-body = dashboardBody()
+# "===========================================
+#                   BODY
+# ==========================================="
+body = dashboardBody(
+  fluidRow(
+    gaugeOutput("gauge")
+  )
+)
 # Getting Started EXample --
 
 
 "===========================================
                 SERVER
 ==========================================="
-server = function(input, output){}
+#I am kind of stuck on this line.  How can I append the input in Rshiny??
+# input <- append(input,df2) #right now just using df2 into server instead
+
+server = function(input, output){
+  
+  output$gauge = renderGauge({
+    finalvalue = input$lwbfinal
+    gauge(finalvalue, 
+          min = df2$lwb[1], 
+          max = df2$lwb[2],
+          sectors = gaugeSectors(success = c(input$lwb[1], df2$lwb[3]),
+                                 danger = c(df2$lwb[3], input$lwb[2]))
+    )
+  })
+}
 
 
 "===========================================
