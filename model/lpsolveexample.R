99fceaf906952fca42c883e78787dd5f2ddd58c7
mlane3 <mlane3@gatech.edu>
2018-03-13 01:27:17 -0400

    Readding contributions
    I am adding credit to the major people who have help so far to contribute to this file.
    
    Co-Authored-By: dandolensky <dandolensky@users.noreply.github.com>
    Co-Authored-By: MamtaBasnet <mamtabasnet@users.noreply.github.com>
    Co-Authored-By: kbrown630 <kbrown630@users.noreply.github.com>

--- model/lpsolveexample.R
+++ model/lpsolveexample.R
@@ -1,22 +1,27 @@
 #An LPsovler example
 library(lpSolve)
-costs <- matrix(c(9, 10, 11, 4, 5, 10, 1, 3, 5, 7, 5, 4), nrow=3)
-nr <- nrow(costs)
-nc <- ncol(costs)
-columns <- t(sapply(1:nc, function(x) rep(c(0, 1, 0), c(nr*(x-1), nr, nr*(nc-x)))))
-rows <- t(sapply(1:nr, function(x) rep(rep(c(0, 1, 0), c(x-1, 1, nr-x)), nc)))
-mod <- lp("max", as.vector(costs), rbind(columns, rows), "<=", rep(1, nr+nc), binary.vec=rep(TRUE, nr*nc))
+# costs <- matrix(c(9, 10, 11, 4, 5, 10, 1, 3, 5, 7, 5, 4), nrow=3)
+# nr <- nrow(costs)
+# nc <- ncol(costs)
+# columns <- t(sapply(1:nc, function(x) rep(c(0, 1, 0), c(nr*(x-1), nr, nr*(nc-x)))))
+# rows <- t(sapply(1:nr, function(x) rep(rep(c(0, 1, 0), c(x-1, 1, nr-x)), nc)))
+# mod <- lp("max", as.vector(costs), rbind(columns, rows), "<=", rep(1, nr+nc), binary.vec=rep(TRUE, nr*nc))
 
-# What else eric and I will go over when we meet ----
+# Current lpSolve version ----
 
 #There is a close form solution 
 #You can take a derivative to figure which point is minimum and maxima
 #df/dx = (7/7+3/3+4/4)/3 = 3/3 = 1
+df2 <- as.data.frame(read.csv("data/overall constrants.csv", skip = 2, row.names = 1)) 
 
 Value = .689
-maxCWB_Z = 1.380706 # maxCWB_Z = min(df_index$CWB_Z)
-minCWB_Z = -1.969282 # minCWB_Z = max(df_index$CWB_Z)
+maxCWB_Z = maxCWB_Z = min(df_index$CWB_Z) # 1.380706
+minCWB_Z = minCWB_Z = max(df_index$CWB_Z) # -1.969282
 ValueZ = (Value*(maxCWB_Z - minCWB_Z)) + minCWB_Z #inverse formula for normalization
+
+source("model/coefficents.R")
+mycoef <- pop.Coef(df0)
+
 
 library(lpSolve)
 library(lpSolveAPI)
@@ -24,27 +29,50 @@ test <- function(){
   # Set the number of vars
   model <- make.lp(0, 14)
   fx <- function(n){
-    ans <- myCoef$coefficients[n]*df2["df0_ave",n] - myCoef$B[n]
+    #This is writting just to show you how CWB Z-score is caculated
+    ans <- mycoef$coeff[n]*df2["df0_ave",n] - mycoef$B[n]
     return(ans) }
+  
   # Define the object function: for Minimize, use -ve
-  set.objfn(model, c(myCoef$gradrate[1],
-                     myCoef$ccrpi[2], 
-                     myCoef$grade3[3],
-                     myCoef$grade8[4],
-                     myCoef$lwb[5],
-                     myCoef$childnohealth[6],
-                     myCoef$childpoverty[7], 
-                     myCoef$povertyrate[8],  
-                     myCoef$housingburden[9], 
-                     myCoef$momsnohs[10], 
-                     myCoef$collegerate[11], 
-                     myCoef$adultsnoedu[12],
-                     myCoef$adultnohealth[13],
-                     myCoef$unemployment[14]))
+  set.objfn(model, c(mycoef$coeff[1],
+                     mycoef$coeff[2], 
+                     mycoef$coeff[3],
+                     mycoef$coeff[4],
+                     mycoef$coeff[5],
+                     mycoef$coeff[6],
+                     mycoef$coeff[7], 
+                     mycoef$coeff[8],  
+                     mycoef$coeff[9], 
+                     mycoef$coeff[10], 
+                     mycoef$coeff[11], 
+                     mycoef$coeff[12],
+                     mycoef$coeff[13],
+                     mycoef$coeff[14]))
                       #Replica of Child well being index but by sub-indexes
+                    #0.003860932 #average Z scofre
   # Add the constraints
-  add.constraint(model, c(7/(7*3), 3/(3*3), 4/(4*3)), "<=", 0.003860932 - ValueZ)
-  # add.constraint(model, c(1, 0, 0), "<", 1) #average x1 = .518 Child
+  # 1/n*coeff
+  add.constraint(model, c(mycoef$coeff[1],mycoef$coeff[2], mycoef$coeff[3],mycoef$coeff[4],
+                          mycoef$coeff[5],mycoef$coeff[6], mycoef$coeff[7], mycoef$coeff[8],  
+                          mycoef$coeff[9], mycoef$coeff[10], 
+                          mycoef$coeff[11], 
+                          mycoef$coeff[12],
+                          mycoef$coeff[13],
+                          mycoef$coeff[14]), "<=",  ValueZ - 0.003860932)
+  add.constraint(model, c(1, 0, 0, 0, 0,0, 0, 0, 0, 0,0, 0, 0, 0), ">", df2$gradrate[1]) #average x1 = .518 Child
+  add.constraint(model, c(0, 1, 0, 0, 0,0, 0, 0, 0, 0,0, 0, 0, 0), ">", df2$ccrpi[1]) #average x1 = .518 Child
+  add.constraint(model, c(0, 0, 1, 0, 0,0, 0, 0, 0, 0,0, 0, 0, 0), ">", df2$grade3[1]) #average x1 = .518 Child
+  add.constraint(model, c(0, 0, 0, 1, 0,0, 0, 0, 0, 0,0, 0, 0, 0), ">", df2$grade8[1]) #average x1 = .518 Child
+  add.constraint(model, c(0, 0, 0, 0, 1,0, 0, 0, 0, 0,0, 0, 0, 0), ">", df2$lbw[1]) #average x1 = .518 Child
+  add.constraint(model, c(0, 0, 0, 0, 0,1, 0, 0, 0, 0,0, 0, 0, 0), ">", df2$childnohealth[1]) #average x1 = .518 Child
+  add.constraint(model, c(0, 0, 0, 0, 0,0, 1, 0, 0, 0,0, 0, 0, 0), ">", df2$childpoverty[1]) #average x1 = .518 Child
+  add.constraint(model, c(0, 0, 0, 0, 0,0, 0, 1, 0, 0,0, 0, 0, 0), ">", df2$povertyrate[1]) 
+  add.constraint(model, c(0, 0, 0, 0, 0,0, 0, 0, 1, 0,0, 0, 0, 0), ">", df2$housingburden[1]) 
+  add.constraint(model, c(0, 0, 0, 0, 0,0, 0, 0, 0, 1, 0, 0, 0, 0), ">", df2$momsnohs[1]) 
+  add.constraint(model, c(0, 0, 0, 0, 0,0, 0, 0, 0, 0, 1, 0, 0, 0), ">", df2$collegerate[1]) 
+  add.constraint(model, c(0, 0, 0, 0, 0,0, 0, 0, 0, 0,0, 1, 0, 0), ">", df2$adultsnoedu[1]) 
+  add.constraint(model, c(0, 0, 0, 0, 0,0, 0, 0, 0, 0,0, 0, 1, 0), ">", df2$adultnohealth[1])
+  add.constraint(model, c(0, 0, 0, 0, 0,0, 0, 0, 0, 0,0, 0, 0, 1), ">", df2$unemployment[1])
   # add.constraint(model, c(0, 0, 1), "<", 1) #average x2 = .500 Family
   # add.constraint(model, c(0, 1, 0), "<", 1) #average x3 = .469 Community
   lp.control(model,sense='min')
@@ -55,8 +83,7 @@ test <- function(){
                      df2$momsnohs[1], df2$collegerate[1], df2$adultsnoedu[1], df2$unemployment[1]), 
              upper=c(df2$gradrate[2], df2$ccrpi[2], df2$X3grade[2], df2$X8grade[2], df2$lwb[2],
                      df2$childnohealth[2], df2$childpoverty[2], df2$povertyrate[2], df2$housingburden[2],
-                     df2$momsnohs[2], df2$collegerate[2], df2$adultsnoedu[2], df2$unemployment[2])
-             ) #***We are using for constraints
+                     df2$momsnohs[2], df2$collegerate[2], df2$adultsnoedu[2], df2$unemployment[2])) #***We are using for constraints
   # Compute the optimized model
   print(solve(model))
   # Get the value of the optimized parameters
