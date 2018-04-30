f5476e7d7d166029c7a851138d294d84d8d44a11
mlane3 <mlane3@gatech.edu>
2018-04-29 20:14:44 -0400

    Minor push just in case Toyin and Eve don't already have it.

--- variablenameslist.R
+++ variablenameslist.R
@@ -5,11 +5,11 @@ variable = c( "gradrate", "ccrpi", "grade3", "grade8", "lbw", "childnohealth",
 "adultsnoedu", "adultnohealth", "unemployment" ),
 number = 1:14,
 title = c( "Graduation Rate", "College and Career Readiness Score", "%
-childern exceed 3rd grade reading standard", "% childern exceed 8th grade math
-standard", "Low Weight Births", "% children wo health insurance", "% childern
-in poverty", "% families not financially stable", "% with housing cost
+Childern exceed 3rd grade reading standard", "% Childern exceed 8th grade math
+standard", "Low Birth Weight", "% Children without health insurance", "% Childern
+in poverty", "% families not financially stable", "% Familes With Housing Cost
 burden", "% of Moms with no high school", "% Adults in Post-Secondary
-Education", "% Adults with no high school", "% adults wo health insurance",
+Education", "% Adults with no high school", "% Adults without health insurance",
 "Unemployment Rate"),
 plotbutton = c(1,rep(0,13)))
 write.csv(variablenamelist,"data/variablenames.csv")
